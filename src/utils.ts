0bb5d1444925562d7f989f121e7531806950d43b
Amine Bl <zazamine8@gmail.com>
2021-11-22 17:21:11 +0100

    fix: check for null object before parse dates

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,7 @@ export function parseDates(obj: DocumentData) {
     const keys = Object.keys(obj);
     keys.forEach(function (key) {
         const value = obj[key];
+        if (!value) return value;
         if (
             typeof value === "object" &&
             "seconds" in value &&
